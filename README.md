abc2300ccbee81ce3db0f4c75b4766d2a6dbce7c
Tej Chajed <chajed@wisc.edu>
2024-02-14 11:32:27 -0600

    Make the demo readable

--- README.md
+++ README.md
@@ -6,3 +6,17 @@ What happens if you don't close the unused ends of a pipe, as `pipe(2)` and
 `pipe(7)` recommend? We can best test this by writing our own C code on both
 ends of the pipe. The GNU utilities are a little too robust to illustrate the
 behavior.
+
+Following the man page, if we close this works:
+
+```sh
+make; ./pipes
+```
+
+Compare this to if we don't close the write end in the child (the consumer):
+
+```sh
+make; timeout 5 ./pipes --no-close-unused
+```
+
+`pipes` will now block forever (the `timeout 5` will stop it after 5 seconds).
--- pipes.c
+++ pipes.c
@@ -1,19 +1,26 @@
 #include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 // producer for the pipe: just writes some constant data
 int parent(int readfd, int writefd) {
   dup2(writefd, STDOUT_FILENO);
-  close(writefd); // this does not seem needed, not sure why? but it is safe
-  close(readfd);  // we also close the unused read end, for a different reason
-  // these printfs will write to the pipe (because of the dup2 above); then the
-  // child will exit which implicitly closes the pipe (stdout)
+  close(writefd); // TODO(tej): this does not seem needed, not sure why? but it
+                  // is safe
+  close(readfd);  // we also close the unused read end (the effect of this isn't
+                  // illustrated in this demo)
+
+  // these printfs will write to the pipe (because of the dup2 above)
   printf("hello world\n");
   printf("hi there\n");
   printf("another line \n");
   printf("the fourth line\n");
+
+  // NOTE: the code below here adds some robustness by getting the child's exit
+  // status, you can ignore it on first read
 
   // we need to flush and close so the child gets EOF and exits before waiting
   // for it (the flush is because we're using the higher-level printf interface
@@ -34,34 +41,46 @@ int parent(int readfd, int writefd) {
   return 0;
 }
 
-int child(int readfd, int writefd) {
-  char buf[1024];
+int child(int readfd, int writefd, bool close_unused) {
   // without this close, the second read will block waiting for more data
   // because we haven't closed the last writefd associated with the pipe
-  close(writefd);
+  if (close_unused) {
+    close(writefd);
+  }
 
   //// this doesn't demonstrate the issue - not sure what head is doing
-  //// specially
+  //// differently
   // dup2(readfd, STDIN_FILENO);
   // close(readfd);
   // execlp("head", "head", "-1", NULL);
 
+  // this will read all of the data
+  char buf[1024];
   int n = read(readfd, buf, sizeof(buf));
-  assert(n > 7);
-  buf[7] = '\0';
+  // print the first 6 bytes
+  assert(n >= 6);
+  buf[6 + 1] = '\0';
   printf("%s\n", buf);
 
-  // this should return 0 to indicate EOF
+  // this should return 0 to indicate EOF, but only if we've properly closed all
+  // writefds for the pipe
   n = read(readfd, buf, sizeof(buf));
+  // this is unexpected
   if (n != 0) {
     fprintf(stderr, "got another %d bytes\n", n);
     return 1;
   }
-  close(readfd);
   return 0;
 }
 
-int main() {
+int main(int argc, char **argv) {
+  bool close_unused = true;
+  if (argc > 1 && strcmp(argv[1], "--no-close-unused") == 0) {
+    close_unused = false;
+  }
+
+  // open a pipe, fork, and run `child` and `parent` as two separate processes
+
   int filedes[2];
   pipe(filedes);
 
@@ -69,7 +88,7 @@ int main() {
   int writefd = filedes[1];
 
   if (fork() == 0) {
-    return child(readfd, writefd);
+    return child(readfd, writefd, close_unused);
   } else {
     return parent(readfd, writefd);
   }
